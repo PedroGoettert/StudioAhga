480b07fe1f06a7e7996ad67524eee52cd530de9f
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2023-07-20 09:24:13 -0300

    Created Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
 import style from './style.module.css'
+
+import project1 from '../Images/Projeto.png'
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className={style.container}>
-      <div>Home</div>
+      <section className={style.content}>
+        <h1 className={style.title}>Seja Bem vindo ao Studio Ahga</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quae
+          deleniti ut iure sit voluptatibus et fuga, laborum nemo, esse sequi
+          molestiae fugit id recusandae, quos culpa? Esse, harum error.
+        </p>
+
+        <div className={style.images}>
+          <Image src={project1} alt="" height={300} />
+          <Image src={project1} alt="" height={300} />
+          <Image src={project1} alt="" height={300} />
+        </div>
+      </section>
     </main>
   )
 }
